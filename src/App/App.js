5fdeda1606f62087218155653da0869bbd9a5f85
richjun <junyoung.yoon@lge.com>
2019-04-27 22:45:48 +0900

    Remove App.less

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,10 @@
 import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import React from 'react';
-
-import css from './App.less';
-import { $L } from '@enact/i18n/$L/$L';
+import $L from '@enact/i18n/$L';
 
 const App = kind({
 	name: 'App',
-
-	styles: {
-		css,
-		className: 'app'
-	},
 
 	render: (props) => (
 		<div {...props}>
